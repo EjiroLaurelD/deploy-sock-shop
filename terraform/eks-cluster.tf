9dff88e74ba2ac6013f1572dddaf371887994281
ejirolaureld <dafelaurele@gmail.com>
2023-03-14 22:39:39 +0100

    updated ec2 file

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -17,8 +17,8 @@ module "eks" {
     eks_managed_node_groups = {
         dev = {
             min_size = 2
-            max_size = 2
-            desired_size = 2
+            max_size = 5
+            desired_size = 4
 
             instance_types = ["t2.medium"]
         }
