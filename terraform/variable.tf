759b8871010688502f1128fa1be439442956e48e
ejirolaureld <dafelaurele@gmail.com>
2023-03-15 10:57:12 +0100

    test

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,15 +1,3 @@
-variable "vpc_cidr_block" {
- description = "VPC CIDR block"
- type = string
-}
-
-variable "private_subnet_cidr_blocks" {
-  description = "Public Subnet cidr block"
-  type        = string
-}
-
-variable "public_subnet_cidr_blocks" {
-  description = "Subnet cidr block"
-  type        = string
-}
-
+variable "vpc_cidr_block" {}
+variable "private_subnet_cidr_blocks" {}
+variable "public_subnet_cidr_blocks" {}
