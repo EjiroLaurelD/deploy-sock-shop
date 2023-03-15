cfe1c6746a8d3504fef33cb530a2c38a6a032017
ejirolaureld <dafelaurele@gmail.com>
2023-03-15 10:52:52 +0100

    test

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,24 +1,24 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "exam3-eks-cluster"
+    cluster_name = "myapp-eks-cluster"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
 
-    vpc_id = module.exam3-vpc.vpc_id
-    subnet_ids = module.exam3-vpc.private_subnets
+    vpc_id = module.myapp-vpc.vpc_id
+    subnet_ids = module.myapp-vpc.private_subnets
 
     tags = {
         environment = "development"
-        application = "exam3"
+        application = "myapp"
     }
 
     eks_managed_node_groups = {
         dev = {
-            min_size = 2
-            max_size = 5
-            desired_size = 4
+            min_size = 1
+            max_size = 3
+            desired_size = 2
 
             instance_types = ["t2.medium"]
         }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,8 +1,8 @@
 data "aws_availability_zones" "azs" {}
-module "exam-vpc" {
+module "myapp-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
   version         = "3.19.0"
-  name            = "exam-vpc"
+  name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
@@ -13,16 +13,16 @@ module "exam-vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/exam-eks-cluster" = "shared"
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/exam-eks-cluster" = "shared"
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
     "kubernetes.io/role/elb"                  = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/exam-eks-cluster" = "shared"
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
     "kubernetes.io/role/internal-elb"         = 1
   }
 }
