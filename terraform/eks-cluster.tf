67c09dda3af8aa820113a5ed55aa9690c2c050f7
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-16 10:07:48 +0100

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,7 +1,11 @@
+locals {
+  cluster_name = "eks-cluster-${var.name}-${data.aws_region.current.name}"
+}
+
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "myapp-eks-cluster"
+    cluster_name = "local.cluster_name"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
