9edd5266fbd0c27fbc29c899834579f58c5c77f9
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-16 14:11:35 +0100

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,6 @@
 variable "vpc_cidr_block" {}
 variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
+variable "kube_config" {}
+variable "namespace" {}
+variable "kube-version" {}
