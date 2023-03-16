bd9dc97f868244cfe750c66a5bec52f9b29a59b8
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-16 15:56:01 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,16 @@
 provider "helm" {
   kubernetes {
-    host = module.eks.endpoint
+    #host = module.eks.endpoint
     token = module.eks.myapp-eks-cluster.token
-    cluster_ca_certificate = base64decode(module.eks.myapp-eks-cluster.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(module.eks.certificate_authority[0].data)
     #config_path = "~/.kube/config"
   }
 }
 
 provider "kubernetes" {
-  host = module.eks.endpoint
+  #host = module.eks.endpoint
   token = module.eks.myapp-eks-cluster.token
-  cluster_ca_certificate = base64decode(module.eks.myapp-eks-cluster.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(module.eks.certificate_authority[0].data)
     
   #config_path = pathexpand(var.kube_config)
 }
