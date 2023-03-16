a4177ecb8f18a20ced3d9ad1840ed3474cab0ba2
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-16 14:56:31 +0100

    Update helm.tf

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,7 +1,9 @@
 resource "helm_release" "kube-prometheus" {
+  depends_on = [kubernetes_namespace.monitoring]
   name       = "kube-prometheus-stackr"
   namespace  = var.namespace
   version    = "45.7.1"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
+  timeout =  2000
 }
