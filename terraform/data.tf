a311733a9cb41855cef0f48978eb02d9e69b8bab
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-16 15:11:32 +0100

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,7 +7,3 @@ data "aws_eks_cluster_auth" "myapp-eks-cluster"{
   depends_on = [aws_eks_cluster.myapp-eks-cluster]
   name = "myapp-eks-cluster"
 }
-
-data "aws_eks_cluster_auth" "myapp-eks-cluster"{
-  pattern = "../kubern*.yaml"
-}
