7d066c9b00f1e05567820ba09076f0e7764037ca
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-16 17:52:38 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,31 +2,21 @@
 
 
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-      command     = "aws"
-    }
-  }
- 
+provider "helm" { 
   #kubernetes {
     #host = module.eks.endpoint
     #token = module.eks.myapp-eks-cluster.token
     #cluster_ca_certificate = base64decode(module.eks.certificate_authority[0].data)
- #   config_path = "~/.kube/config"
+    config_path = "~/.kube/config"
   #}
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+ # host                   = data.aws_eks_cluster.cluster.endpoint
+  #cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  #token                  = data.aws_eks_cluster_auth.cluster.token
   #host = module.eks.endpoint
   #token = module.eks.myapp-eks-cluster.token
   #cluster_ca_certificate = base64decode(module.eks.certificate_authority[0].data)  
-  #config_path = pathexpand(var.kube_config)
+  config_path = pathexpand(var.kube_config)
 }
