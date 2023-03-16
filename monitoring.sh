079709151b03e2914ae950e16a69349e0175b634
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-03-16 20:07:38 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,5 +1,20 @@
 #!/bin/bash 
 
+#install helm
+sudo yum install helm
+
+#install kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo yum install -y kubectl
+
+#install kube-prometheus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install my-kube-prometheus-stack prometheus-community/kube-prometheus-stack --version 36.6.2
